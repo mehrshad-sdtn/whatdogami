94b95b8d541c568db8e7231346abc2f46017a497
Mehrshad Saadatinia <saadatiniamehrshad@gmail.com>
2022-10-22 00:21:52 +0330

    Update classifier.py

--- classifier.py
+++ classifier.py
@@ -13,12 +13,11 @@ def get_class_name(label_index):
 
 def get_image_prediction(path):
   model = tf.keras.models.load_model(os.path.join('model','Xception_model.h5'))
-  img = tf.keras.preprocessing.image.load_img(os.path.join('static', 'images', 'dober.jfif'), color_mode="rgb", target_size=(180, 180, 3))
-  #img_arr = (tf.keras.preprocessing.image.img_to_array(img))/255
-  #img_arr = np.array([img_arr])
-  #predictions = model.predict(img_arr)
-  #label = np.argmax(predictions, axis=-1)
-  #index = np.squeeze(label)
-  index = 29
+  img = tf.keras.preprocessing.image.load_img(path, color_mode="rgb", target_size=(180, 180, 3))
+  img_arr = (tf.keras.preprocessing.image.img_to_array(img))/255
+  img_arr = np.array([img_arr])
+  predictions = model.predict(img_arr)
+  label = np.argmax(predictions, axis=-1)
+  index = np.squeeze(label)
   return get_class_name(index)
 
